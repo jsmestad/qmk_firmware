a60baaf535172bd3801a7177809952f3ce345894
Justin Smestad <justin.smestad@gmail.com>
2018-10-22 21:16:34 -0600

    Swap a few keys back to original placement

--- keyboards/iris/keymaps/jsmestad/keymap.c
+++ keyboards/iris/keymaps/jsmestad/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
      LSFT, Z  , X  , C  , V  , B  ,LALT,     BSPC, N  , M  ,COMM,DOT ,SLSH,RASE,
   //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
-                       LOWR,SPC ,SPC ,         ENTS,LBRC,RBRC
+                       LOWR,SPC ,ENTS,         SPC ,LBRC,RBRC
   //                  `----+----+----'        `----+----+----'
   ),
 
