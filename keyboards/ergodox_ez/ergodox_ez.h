877ee704e6df6c49d9d67398765acd7cf39aefb3
Justin Smestad <justin.smestad@gmail.com>
2018-12-09 11:09:40 -0700

    Add KC shortcut keymap

--- keyboards/ergodox_ez/ergodox_ez.h
+++ keyboards/ergodox_ez/ergodox_ez.h
@@ -214,14 +214,35 @@ inline void ergodox_led_all_set(uint8_t n)
     { L05, L15, L25, L35, KC_NO, L55 },   \
     { L06, L16, KC_NO, L36, KC_NO, L56 }, \
                                           \
-    { R00, R10, KC_NO, R30,KC_NO, R50 },  \
-    { R01, R11, R21, R31,KC_NO, R51 },    \
+    { R00, R10, KC_NO, R30, KC_NO, R50 }, \
+    { R01, R11, R21, R31, KC_NO, R51 },   \
     { R02, R12, R22, R32, R42, R52 },     \
     { R03, R13, R23, R33, R43, R53 },     \
     { R04, R14, R24, R34, R44, R54 },     \
     { R05, R15, R25, R35, R45, R55 },     \
     { R06, R16, R26, R36, R46, KC_NO }    \
     }
+/*  ---------- LEFT HAND -----------   ---------- RIGHT HAND ---------- */
+#define LAYOUT_ergodox_pretty_kc(                                        \
+    L00,L01,L02,L03,L04,L05,L06,           R00,R01,R02,R03,R04,R05,R06,  \
+    L10,L11,L12,L13,L14,L15,L16,           R10,R11,R12,R13,R14,R15,R16,  \
+    L20,L21,L22,L23,L24,L25,                   R21,R22,R23,R24,R25,R26,  \
+    L30,L31,L32,L33,L34,L35,L36,           R30,R31,R32,R33,R34,R35,R36,  \
+    L40,L41,L42,L43,L44,                           R42,R43,R44,R45,R46,  \
+                            L55,L56,   R50,R51,                          \
+                                L54,   R52,                              \
+                        L53,L52,L51,   R55,R54,R53 )                     \
+                                          \
+    LAYOUT_ergodox_pretty( \
+        KC_##L00,KC_##L01,KC_##L02,KC_##L03,KC_##L04,KC_##L05,KC_##L06,                   KC_##R00,KC_##R01,KC_##R02,KC_##R03,KC_##R04,KC_##R05,KC_##R06,  \
+        KC_##L10,KC_##L11,KC_##L12,KC_##L13,KC_##L14,KC_##L15,KC_##L16,                   KC_##R10,KC_##R11,KC_##R12,KC_##R13,KC_##R14,KC_##R15,KC_##R16,  \
+        KC_##L20,KC_##L21,KC_##L22,KC_##L23,KC_##L24,KC_##L25,                                     KC_##R21,KC_##R22,KC_##R23,KC_##R24,KC_##R25,KC_##R26,  \
+        KC_##L30,KC_##L31,KC_##L32,KC_##L33,KC_##L34,KC_##L35,KC_##L36,                   KC_##R30,KC_##R31,KC_##R32,KC_##R33,KC_##R34,KC_##R35,KC_##R36,  \
+        KC_##L40,KC_##L41,KC_##L42,KC_##L43,KC_##L44,                                                       KC_##R42,KC_##R43,KC_##R44,KC_##R45,KC_##R46,  \
+                                                              KC_##L55,KC_##L56, KC_##R50,KC_##R51,         \
+                                                                       KC_##L54, KC_##R52,                  \
+                                                     KC_##L53,KC_##L52,KC_##L51, KC_##R55,KC_##R54,KC_##R53 \
+    )
 
 /*  ---------- LEFT HAND -----------   ---------- RIGHT HAND ---------- */
 #define LAYOUT_ergodox_pretty_80(                                        \
