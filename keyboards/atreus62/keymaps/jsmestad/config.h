16cd9a06d830d4f5c4dae2178f1a52dc9b2d535c
Justin Smestad <justin.smestad@gmail.com>
2018-12-22 20:36:01 -0700

    Atreus62 configuration with Elite C

--- keyboards/atreus62/keymaps/jsmestad/config.h
+++ keyboards/atreus62/keymaps/jsmestad/config.h
@@ -1,3 +1,11 @@
 #pragma once
-#define RGB_DI_PIN B7
+
+/* With Elite C, there are extra unused pins */
+#define UNUSED_PINS { F0, F1, C7, B0, D5 }
+
+#define USB_MAX_POWER_CONSUMPTION 500
+
+/* ws2812 RGB LED */
+#define RGB_DI_PIN B7 // Requires Elite C
 #define RGBLED_NUM 6
+#define RGBLIGHT_SLEEP
--- keyboards/atreus62/keymaps/jsmestad/keymap.c
+++ keyboards/atreus62/keymaps/jsmestad/keymap.c
@@ -2,49 +2,79 @@
 // This is the canonical layout file for the Quantum project. If you want to add another keyboard,
 
 #include QMK_KEYBOARD_H
+#include "atreus62.h"
+#include "action_layer.h"
+#include "eeconfig.h"
+
+extern keymap_config_t keymap_config;
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
-#define _DEFAULT 0
-#define _NAV 1
-#define _RESET 2
+#define _QWERTY 0
+#define _RAISE 1
+#define _ADJUST 2
+
+enum custom_keycodes {
+  QWERTY = SAFE_RANGE,
+  RAISE,
+  /* LOWER, */
+  /* RAISE, */
+  ADJUST,
+  /* GAMING, */
+};
+
+#define KC_ KC_TRNS
+#define _______ KC_TRNS
+
+#define KC_NAV MO(_RAISE)
+#define KC_PC TO(0)
+#define KC_ADJ MO(_ADJUST)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_DEFAULT] = LAYOUT( /* qwerty */
-    KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS ,
-    KC_BSLS,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_RBRC ,
-    KC_TAB,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT ,
-    KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_LBRC ,
-    KC_LCTL,  KC_LGUI, KC_LALT, KC_GRV,  MO(_NAV),KC_BSPC, KC_DELT,  KC_ENT,   KC_SPC,  KC_EQL,  KC_MINS, KC_QUOT, KC_ENT,  KC_RGUI
+  [_QWERTY] = LAYOUT_kc(
+    //,----+----+----+----+----+----.           .----+----+----+----+----+----,
+       ESC , 1  , 2  , 3  , 4  , 5  ,             6  , 7  , 8  , 9  , 0  ,MINS,
+    //|----+----+----+----+----+----|           |----+----+----+----+----+----|
+       BSLS, Q  , W  , E  , R  , T  ,             Y  , U  , I  , O  , P  ,RBRC,
+    //|----+----+----+----+----+----|           |----+----+----+----+----+----|
+       TAB , A  , S  , D  , F  , G  ,             H  , J  , K  , L  ,SCLN,QUOT,
+    //|----+----+----+----+----+----|           |----+----+----+----+----+----|
+       LSFT, Z  , X  , C  , V  , B  ,             N  , M  ,COMM,DOT ,SLSH,LBRC,
+    //|----+----+----+----+----+----+----| |----+----+----+----+----+----+----|
+       LCTL,LGUI,LALT,GRV ,NAV ,BSPC,DELT,  ENT ,SPC ,EQL ,MINS,QUOT,ENT ,RGUI
+    //,----+----+----+----+----+----+----. .----+----+----+----+----+----+----,
   ),
 
-  [_NAV] = LAYOUT(
-    TO(_DEFAULT),  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11  ,
-    KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_F12,  KC_TRNS, KC_UP,   KC_TRNS, KC_TRNS, KC_TRNS ,
-    KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS ,
-    TO(_RESET),    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS ,
-    KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+  [_RAISE] = LAYOUT_kc(
+    //,----+----+----+----+----+----.           .----+----+----+----+----+----,
+       PC  , F1 , F2 , F3 , F4 , F5 ,             F6 , F7 , F8 , F9 , F10, F11,
+    //|----+----+----+----+----+----|           |----+----+----+----+----+----|
+           ,    ,    ,    ,    ,    ,                ,    ,    ,    ,    ,    ,
+    //|----+----+----+----+----+----|           |----+----+----+----+----+----|
+           ,    ,    ,    ,    ,    ,                ,    ,    ,    ,    ,    ,
+    //|----+----+----+----+----+----|           |----+----+----+----+----+----|
+       ADJ ,    ,    ,    ,    ,                ,    ,    ,    ,    ,    ,    ,
+    //|----+----+----+----+----+----+----| |----+----+----+----+----+----+----|
+           ,    ,    ,    ,    ,    ,    ,      ,    ,    ,    ,    ,    ,
+    //,----+----+----+----+----+----+----. .----+----+----+----+----+----+----,
   ),
 
-  [_RESET] = LAYOUT(
-    TO(_DEFAULT),  KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,                     KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO   ,
-    KC_NO  ,       KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,                     KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO   ,
-    KC_NO  ,       KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,                     KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO   ,
-    KC_NO  ,       KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,                     KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO   ,
-    KC_NO  ,       KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,  KC_NO  ,  KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , RESET
+  [_ADJUST] = LAYOUT(
+    //,----+----+----+----+----+----.           .----+----+----+----+----+----,
+       _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
+    //|----+----+----+----+----+----|           |----+----+----+----+----+----|
+       RGB_TOG,RGB_MOD,RGB_HUI,RGB_SAI,RGB_VAI,_______,_______,_______,_______,_______,_______,_______,
+    //|-------+-------+-------+-------+-------+-------|           |----+----+----+----+----+----|
+       _______,RGB_RMOD,RGB_HUD,RGB_SAD,RGB_VAD,_______,_______,_______,_______,_______,_______,_______,
+    //|----+----+----+----+----+----|           |----+----+----+----+----+----|
+       _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
+    //|----+----+----+----+----+----+----| |----+----+----+----+----+----+----|
+       _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,RESET
+           /* ,    ,    ,    ,    ,    ,    ,      ,    ,    ,    ,    ,    ,RESET */
+    //,----+----+----+----+----+----+----. .----+----+----+----+----+----+----,
   )
-
-  /*
-  [_TRNS] = LAYOUT(
-    KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS ,
-    KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS ,
-    KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS ,
-    KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS ,
-    KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
-  ),
-  */
 };
 
 
@@ -52,6 +82,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 const uint16_t PROGMEM fn_actions[] = {
 
 };
+
+void persistent_default_layer_set(uint16_t default_layer) {
+  eeconfig_update_default_layer(default_layer);
+  default_layer_set(default_layer);
+}
+
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
   // MACRODOWN only works in this function
