d7bda22794efb172772d1906e76979aa72fbb3f9
Justin Smestad <justin.smestad@gmail.com>
2019-05-02 11:33:19 +0200

    Add permissive hold

--- keyboards/ergodox_ez/keymaps/jsmestad/config.h
+++ keyboards/ergodox_ez/keymaps/jsmestad/config.h
@@ -1,3 +1,6 @@
 #pragma once
 
 #define QMK_KEYS_PER_SCAN 4
+
+// makes tap and hold keys trigger the hold if another key is pressed before releasing, even if it hasn't hit the TAPPING_TERM
+#define PERMISSIVE_HOLD
