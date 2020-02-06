dbae589bdca1e85e503ebdaa9fe10223cc1464cd
Justin Smestad <justin.smestad@gmail.com>
2020-02-06 09:46:23 -0700

    Updates for where LGUI is

--- keyboards/kinesis/keymaps/jsmestad/keymap.c
+++ keyboards/kinesis/keymaps/jsmestad/keymap.c
@@ -7,6 +7,8 @@ extern keymap_config_t keymap_config;
 
 #define KC_ESCC MT(MOD_LCTL, KC_ESC)   // Control (hold), Escape (tap)
 #define KC_LOWR MO(_LOWER)
+#define KC_DELL LT(_LOWER, KC_DEL)   // Lower Layer (hold), Delete (tap)
+#define KC_ENTL LT(_LOWER, KC_ENT)   // Lower Layer (hold), Return (tap)
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
@@ -46,10 +48,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                                                                      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSLS,
     KC_ESCC,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                                                                      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
     KC_LSPO,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                                                                      KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSPC,
-              KC_GRV,   KC_INS,   KC_LEFT,  KC_LOWR,                                                                                       KC_UP,    KC_DOWN,  KC_LBRC,  KC_RBRC,
-                                                      KC_LCTL,  KC_LALT,                                               KC_RGUI,  KC_RCTL,
-                                                                KC_HOME,                                               KC_PGUP,
-                                            KC_BSPC,  KC_DEL,   KC_END,                                                KC_PGDN, KC_ENTER, KC_SPC),
+            KC_GRV,   KC_INS,  KC_LEFT,  KC_RGHT,                                                                                          KC_UP,    KC_DOWN,  KC_LBRC,  KC_RBRC,
+                                                      KC_LCTL,  KC_LALT,                                               KC_RALT, KC_RCTL,
+                                                                KC_LALT,                                               KC_PGUP,
+                                            KC_BSPC,  KC_DELL,  KC_LGUI,                                               KC_PGDN, KC_ENTL, KC_SPC),
 
   [_LOWER] = LAYOUT_pretty(
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
